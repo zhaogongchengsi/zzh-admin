e24230d21b1adea0459641098d37c5e1cc11bced
zhaogongchengsi <zzh1586169624@163.com>
2022-02-09 17:16:47 +0800

    增加app 配置

---
+++ blog-cms/app/index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  // win.loadFile("../index.html");
+  win.loadURL("http://localhost:3000/");
+}
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
