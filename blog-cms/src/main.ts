4d656a5910441a3748f963cc65d8faee0eecda7b
zhaogongchengsi <zzh1586169624@163.com>
2022-02-10 08:38:07 +0800

    初始化

--- blog-cms/src/main.ts
+++ blog-cms/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
 import { createPinia } from 'pinia'
-import router from '@/router/index.js';
+import router from '@/router/index.ts';
 
 const app = createApp(App)
 app.use(ArcoVue, {
